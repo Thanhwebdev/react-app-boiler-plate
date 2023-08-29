d9aac27c340c58c6f21c5b020cdb323112cb12d3
Thanhwebdev <thanhtdo1983@gmail.com>
2023-08-29 11:22:17 -0700

    update commons to use generics

--- src/common/locale/en-us/index.ts
+++ src/common/locale/en-us/index.ts
@@ -1,35 +1,4 @@
 export default {
-  configFields: {
-    description:
-      "Enter your Beebox project credentials. You can obtain these from the Beebox dashboard settings page. <br /> Verify the connection. Hit <strong>Save</strong>, then, <strong>Open Stack</strong>.",
-    beeboxSettings: {
-      title: "Beebox credentials",
-      apiUrl: {
-        label: "API Url",
-      },
-      projectKey: {
-        label: "API Project Key",
-      },
-      login: {
-        label: "API Login",
-      },
-      password: {
-        label: "API Password",
-      },
-      connectBtnLabel: "Verify Connection",
-    },
-    projectDetails: {
-      title: "Project Details",
-      sourceLanguage: {
-        fieldLabel: "Source Language",
-      },
-      targetLanguage: {
-        fieldLabel: "Target Language(s)",
-      },
-      sourceLanguageRTL: {
-        fieldLabel: "Source Language RTL",
-        helpText: "Right to left language, e.g., Arabic and Hebrew.",
-      },
-    },
-  },
+  title: 'hello world!',
+  description: 'Welcome to free react typescript boiler plate!'
 };
