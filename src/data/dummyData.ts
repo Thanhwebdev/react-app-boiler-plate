16d2d8e624ef72f6c0f4b94180edc26bdeece9c6
Thanhwebdev <thanhtdo1983@gmail.com>
2023-08-29 11:26:56 -0700

    add dummy data to data

---
+++ src/data/dummyData.ts
@@ -0,0 +1,17 @@
+export const DUMMYDATA: any[] = [
+  {
+    id: 1,
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    userId: 1
+  },
+  {
+    id: 2,
+    title: "qui est esse",
+    userId: 1
+  },
+  {
+    id: 3,
+    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    userId: 1
+  }
+]
